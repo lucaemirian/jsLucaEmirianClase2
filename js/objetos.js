0fb46261e787c44e09b7df906df7784520e17394
lucaemirian <lucaemirian@gmail.com>
2021-12-11 21:14:36 -0300

    metodos

--- js/objetos.js
+++ js/objetos.js
@@ -1,15 +1,36 @@
-const usuario = {
-    nombre: 'Luca',
+function Usuario(nombre, edad, mail) {
+    this.nombre = nombre;
 
-    edad: 25,
+    this.edad = edad;
 
-    email: 'lucaemirian@gmail.com';
+    this.mail = mail;
 }
 
-console.log ( persona )
+    registro() {
+        console.log('¡Bienvenido' this.nombre', tu registro fue exitoso!')
+}
+
+
+const usuario1 = new Usuario('Luca', 25, 'lucaemirian@gmail.com')
+
+const usuario2 = new Usuario('Pablo', 27, 'pablito@gmail.com')
+
+const usuario3 = new Usuario('Marcos', 30, 'marc01hotmail.com')
+
+const usuario4 = new Usuario('Martin', 15, 'tincho@gmail.com')
 
 
 
+
+
+
+console.log ( usuario1 )
+console.log ( usuario2 )
+console.log ( usuario3 )
+console.log ( usuario4 )
+
+const values = Object.values (usuario3)
+console.log(values)
 
 
 
